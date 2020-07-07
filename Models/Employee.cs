2d6fa130af4f89590135f577bf8352aff2fe87a7
romarius-by <romarius-by@users.noreply.github.com>
2020-07-07 22:47:09 +0300

    Fixed bug with data in Edit mode

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,8 @@ namespace RSOE.Models
         public int EmployeeId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EmploymentDate { get; set; }
         public int PositionId { get; set; }
         public int CompanyId { get; set; }
