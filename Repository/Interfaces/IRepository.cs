6ee4f886c5079c1606445296eec726794732056c
romarius-by <romarius-by@users.noreply.github.com>
2020-07-07 22:43:45 +0300

    Added comments

--- Repository/Interfaces/IRepository.cs
+++ Repository/Interfaces/IRepository.cs
@@ -7,11 +7,11 @@ namespace RSOE.Repository.Interfaces
 {
     public interface IRepository<T>
     {
-        T GetById(int id);
-        void Create(T item);
-        void Update(T item);
-        void Delete(T item);
+        T GetById(int id); // Получает объект из базы данных по Id
+        void Create(T item); // Добавляет объект в базу данных
+        void Update(T item); // Изменяет объект в базе данных
+        void Delete(T item); // Удаляет объект из базы данных
 
-        IEnumerable<T> GetAll();
+        IEnumerable<T> GetAll(); // Получает список объектов из таблицы
     }
 }
